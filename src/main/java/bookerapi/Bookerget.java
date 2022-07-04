d1c282a71754151fbcd5a3a713b657c234a73d52
sreenathmohanan <88075098+sreenathmohanan@users.noreply.github.com>
2022-07-04 18:57:01 -0500

    first commit

---
+++ src/main/java/bookerapi/Bookerget.java
@@ -0,0 +1,42 @@
+package bookerapi;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+
+public class Bookerget
+{
+    @Test
+	public void getReq()
+	{
+		
+		RestAssured
+		.given()
+		  .log().all()
+		  .baseUri("https://restful-booker.herokuapp.com/")
+          .basePath("booking/{id}")
+          .pathParam("id", 3321)
+		  .contentType(ContentType.JSON)
+		.when()
+		  .get()
+		.then()
+		  .assertThat()
+		  .statusCode(200)
+		  .log().all();
+		  
+		  
+		  
+		  
+		  
+		 
+		
+		
+		
+		
+	}
+	
+	
+	
+}
